7aee7a4ed1ac8aff605366b6fac16546a8fa62c7
Chetan77 <cchoudhari1@gmail.com>
2020-02-21 14:50:49 +0530

    [Chetan]Add. added implementation of UseCase1 to pass message and analysing the mood

--- MoodAnalyser/MoodAnalyser/UseCase1.cs
+++ MoodAnalyser/MoodAnalyser/UseCase1.cs
@@ -4,15 +4,30 @@ namespace MoodAnalyser
 {
     public class UseCase1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void MoodAnalysis_ToTest_InSadMood()
         {
-            Assert.Pass();
+            string actual = Moodanalyser.AnalyseMood("i am in sad mood");
+            Assert.AreEqual("SAD", actual);
         }
+        [Test]
+        public void MoodAnalysis_ToTest_InHappyMood()
+        {
+            string actual = Moodanalyser.AnalyseMood("i am in happy mood");
+            Assert.AreEqual("HAPPY", actual);
+        }
+        [Test]
+        public void MoodAnalysis_ToTest_InAnyMood()
+        {
+            string actual = Moodanalyser.AnalyseMood("i am in any mood");
+            Assert.AreEqual("HAPPY", actual);
+        }
+       /* [Test]
+        public void MoodAnalyser_passedInConstructor()
+        {
+            Moodanalyser moodAnalyser = new Moodanalyser("i am in happy mood");
+            string actual = moodAnalyser.AnalyseMoo();
+            Assert.AreEqual("HAPPY", actual);
+        }*/
     }
 }
